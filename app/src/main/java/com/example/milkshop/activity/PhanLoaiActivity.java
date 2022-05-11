5b54fce3b36a5ea138fcf301be6720af284b83c4
letiendat1002 <letiendat1002@gmail.com>
2022-05-11 08:33:25 +0700

    Rut gon activity + Bai 13: Tao Loadmore

---
+++ app/src/main/java/com/example/milkshop/activity/PhanLoaiActivity.java
@@ -0,0 +1,170 @@
+package com.example.milkshop.activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.milkshop.R;
+import com.example.milkshop.adapter.PhanLoaiAdapter;
+import com.example.milkshop.model.SanPham;
+import com.example.milkshop.retrofit.ApiBanHang;
+import com.example.milkshop.retrofit.RetrofitClient;
+import com.example.milkshop.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class PhanLoaiActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    CompositeDisposable compositeDisposable;
+    ApiBanHang apiBanHang;
+    int page;
+    int loai;
+    List<SanPham> mangsanpham;
+    PhanLoaiAdapter phanLoaiAdapter;
+    LinearLayoutManager linearLayoutManager;
+    Handler handler;
+    boolean isLoading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loai = getIntent().getIntExtra("loai", 1);
+        switch (loai) {
+            case 1:
+                setContentView(R.layout.activity_sua_tuoi);
+                recyclerView = findViewById((R.id.recycleview_suatuoi));
+                break;
+            case 2:
+                setContentView(R.layout.activity_sua_bot);
+                recyclerView = findViewById((R.id.recycleview_suabot));
+                break;
+            case 3:
+                setContentView(R.layout.activity_sua_dac);
+                recyclerView = findViewById((R.id.recycleview_suadac));
+                break;
+        }
+        addControls();
+        ActionToolBar();
+        getData(page);
+        addEvents();
+    }
+
+    private void addControls() {
+        compositeDisposable = new CompositeDisposable();
+        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+        page = 1;
+        toolbar = findViewById(R.id.toolbar);
+
+        linearLayoutManager = new LinearLayoutManager(
+                this,
+                LinearLayoutManager.VERTICAL,
+                false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        mangsanpham = new ArrayList<>();
+
+        handler = new Handler();
+        isLoading = false;
+    }
+
+    private void ActionToolBar() {
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void getData(int page) {
+        compositeDisposable.add(apiBanHang.getPhanLoai(page, loai)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        sanPhamModel -> {
+                            if (sanPhamModel.isSuccess()) {
+                                if (phanLoaiAdapter == null) { // Không có data
+                                    mangsanpham = sanPhamModel.getResult();
+                                    phanLoaiAdapter = new PhanLoaiAdapter(getApplicationContext(), mangsanpham);
+                                    recyclerView.setAdapter(phanLoaiAdapter);
+                                } else { // Đã có data
+                                    int pos = mangsanpham.size() - 1;
+                                    int addAmount = sanPhamModel.getResult().size();
+                                    for (int i = 0; i < addAmount; ++i) {
+                                        mangsanpham.add(sanPhamModel.getResult().get(i));
+                                    }
+                                    phanLoaiAdapter.notifyItemRangeInserted(pos, addAmount);
+                                }
+                            } else {
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "No More Product Available",
+                                        Toast.LENGTH_SHORT).show();
+                                isLoading = true;
+                            }
+                        },
+                        throwable -> Toast.makeText(
+                                getApplicationContext(),
+                                "Không kết nối được với server\n" + throwable.getMessage(),
+                                Toast.LENGTH_LONG
+                        ).show()
+                ));
+    }
+
+    private void addEvents() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (!isLoading) {
+                    if (linearLayoutManager.findLastCompletelyVisibleItemPosition() == mangsanpham.size() - 1) {
+                        isLoading = true;
+                        loadMore();
+                    }
+                }
+            }
+        });
+    }
+
+    private void loadMore() {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                // add null
+                mangsanpham.add(null);
+                phanLoaiAdapter.notifyItemInserted(mangsanpham.size() - 1);
+            }
+        });
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mangsanpham.remove(mangsanpham.size() - 1);
+                phanLoaiAdapter.notifyItemRemoved(mangsanpham.size());
+                page += 1;
+                getData(page);
+                phanLoaiAdapter.notifyDataSetChanged();
+                isLoading = false;
+            }
+        }, 2000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
+    }
+}
--- app/src/main/java/com/example/milkshop/adapter/PhanLoaiAdapter.java
+++ app/src/main/java/com/example/milkshop/adapter/PhanLoaiAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,7 +18,9 @@ import com.example.milkshop.model.SanPham;
 import java.text.DecimalFormat;
 import java.util.List;
 
-public class PhanLoaiAdapter extends RecyclerView.Adapter<PhanLoaiAdapter.MyViewHolder> {
+public class PhanLoaiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    public static final int VIEW_TYPE_DATA = 0;
+    public static final int VIEW_TYPE_LOADING = 1;
     Context context;
     List<SanPham> array;
 
@@ -28,25 +31,44 @@ public class PhanLoaiAdapter extends RecyclerView.Adapter<PhanLoaiAdapter.MyView
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(
-                R.layout.item_phanloai,
-                parent,
-                false
-        );
-        return new MyViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_DATA) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.item_phanloai,
+                    parent,
+                    false
+            );
+            return new MyViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.item_loading,
+                    parent,
+                    false
+            );
+            return new LoadingViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        SanPham sanPham = array.get(position);
-        holder.tensp.setText(sanPham.getTensp());
-        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
-        String price = decimalFormat.format(Double.parseDouble(sanPham.getGiasp())) + "đ";
-        holder.giasp.setText(price);
-        holder.mota.setText(sanPham.getMota());
-        Glide.with(context).load(sanPham.getHinhanh()).into(holder.hinhanh);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof MyViewHolder) {
+            MyViewHolder myViewHolder = (MyViewHolder) holder;
+            SanPham sanPham = array.get(position);
+            myViewHolder.tensp.setText(sanPham.getTensp());
+            DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+            String price = decimalFormat.format(Double.parseDouble(sanPham.getGiasp())) + "đ";
+            myViewHolder.giasp.setText(price);
+            myViewHolder.mota.setText(sanPham.getMota());
+            Glide.with(context).load(sanPham.getHinhanh()).into(myViewHolder.hinhanh);
+        } else {
+            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
+            loadingViewHolder.progressBar.setIndeterminate(true);
+        }
+    }
 
+    @Override
+    public int getItemViewType(int position) {
+        return array.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_DATA;
     }
 
     @Override
@@ -54,6 +76,15 @@ public class PhanLoaiAdapter extends RecyclerView.Adapter<PhanLoaiAdapter.MyView
         return array.size();
     }
 
+
+    public static class LoadingViewHolder extends RecyclerView.ViewHolder {
+        ProgressBar progressBar;
+
+        public LoadingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.progressbar);
+        }
+    }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         TextView tensp, giasp, mota;
@@ -67,4 +98,6 @@ public class PhanLoaiAdapter extends RecyclerView.Adapter<PhanLoaiAdapter.MyView
             hinhanh = itemView.findViewById(R.id.itemphanloai_image);
         }
     }
+
+
 }
