fb2da030b1316d6ac4a125ba95d25e6e9947f1b3
letiendat1002 <letiendat1002@gmail.com>
2022-05-13 10:06:21 +0700

    Thay doi spinner thanh cong tru so luong

--- app/src/main/java/com/example/milkshop/activity/ChiTietActivity.java
+++ app/src/main/java/com/example/milkshop/activity/ChiTietActivity.java
@@ -2,11 +2,9 @@ package com.example.milkshop.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,10 +21,9 @@ import java.text.DecimalFormat;
 import java.util.Objects;
 
 public class ChiTietActivity extends AppCompatActivity {
-    TextView tvTenSpChiTiet, tvGiaSpChiTiet, tvMoTaChiTiet;
+    TextView tvTenSpChiTiet, tvGiaSpChiTiet, tvMoTaChiTiet, tvSoLuongChiTiet;
     Button btnAddToCartChiTiet;
-    ImageView ivHinhAnhChiTiet;
-    Spinner spinnerChiTiet;
+    ImageView ivHinhAnhChiTiet, ivTruChiTiet, ivCongChiTiet;
     Toolbar toolbarChiTiet;
     SanPham sanPham;
     NotificationBadge badgeChiTiet;
@@ -47,9 +44,11 @@ public class ChiTietActivity extends AppCompatActivity {
         tvTenSpChiTiet = findViewById(R.id.tvTenSp_chitiet);
         tvGiaSpChiTiet = findViewById(R.id.tvGiaSp_chitiet);
         tvMoTaChiTiet = findViewById(R.id.tvMoTa_chitiet);
-        spinnerChiTiet = findViewById(R.id.spinner_chitiet);
+        tvSoLuongChiTiet = findViewById(R.id.tvSoLuong_chitiet);
         btnAddToCartChiTiet = findViewById(R.id.btnAddToCart_chitiet);
         ivHinhAnhChiTiet = findViewById(R.id.ivHinhAnh_chitiet);
+        ivTruChiTiet = findViewById(R.id.ivTru_chitiet);
+        ivCongChiTiet = findViewById(R.id.ivCong_chitiet);
         toolbarChiTiet = findViewById(R.id.toolbar_chitiet);
         badgeChiTiet = findViewById(R.id.badge_chitiet);
         frameLayoutIconGioHangChiTiet = findViewById(R.id.frame_icon_giohang_chitiet);
@@ -64,7 +63,7 @@ public class ChiTietActivity extends AppCompatActivity {
     private void addData() {
         if (Utils.gioHangList != null) {
             int totalItems = 0;
-            for (int i = 0; i < Utils.gioHangList.size(); i++){
+            for (int i = 0; i < Utils.gioHangList.size(); i++) {
                 totalItems += Utils.gioHangList.get(i).getSoluong();
             }
             badgeChiTiet.setText(String.valueOf(totalItems));
@@ -76,13 +75,6 @@ public class ChiTietActivity extends AppCompatActivity {
         DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
         String price = decimalFormat.format(Double.parseDouble(sanPham.getGiasp())) + "₫";
         tvGiaSpChiTiet.setText(price);
-        Integer[] itemSpinner = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        ArrayAdapter<Integer> spinnerAdapter = new ArrayAdapter<>(
-                this,
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-                itemSpinner
-        );
-        spinnerChiTiet.setAdapter(spinnerAdapter);
     }
 
     private void addEvents() {
@@ -91,12 +83,26 @@ public class ChiTietActivity extends AppCompatActivity {
                 getApplicationContext(),
                 GioHangActivity.class)
         ));
+
+        ivTruChiTiet.setOnClickListener(view -> {
+            int amount = Integer.parseInt(tvSoLuongChiTiet.getText().toString());
+            if (amount == 1)
+                ivTruChiTiet.setClickable(false);
+            tvSoLuongChiTiet.setText(String.valueOf(amount - 1));
+        });
+
+        ivCongChiTiet.setOnClickListener(view -> {
+            int amount = Integer.parseInt(tvSoLuongChiTiet.getText().toString());
+            if (amount == 0)
+                ivTruChiTiet.setClickable(true);
+            tvSoLuongChiTiet.setText(String.valueOf(amount + 1));
+        });
     }
 
     private void themGioHang() {
         if (Utils.gioHangList.size() > 0) {
             boolean flag = false;
-            int soluong = Integer.parseInt(spinnerChiTiet.getSelectedItem().toString());
+            int soluong = Integer.parseInt(tvSoLuongChiTiet.getText().toString());
             for (int i = 0; i < Utils.gioHangList.size(); i++) {
                 if (Utils.gioHangList.get(i).getId() == sanPham.getId()) {
                     Utils.gioHangList.get(i).setSoluong(soluong + Utils.gioHangList.get(i).getSoluong());
@@ -119,7 +125,7 @@ public class ChiTietActivity extends AppCompatActivity {
             }
 
         } else {
-            int soluong = Integer.parseInt(spinnerChiTiet.getSelectedItem().toString());
+            int soluong = Integer.parseInt(tvSoLuongChiTiet.getText().toString());
             long gia = Long.parseLong(sanPham.getGiasp()) * soluong;
             GioHang gioHang = new GioHang();
             gioHang.setGiasp(gia);
@@ -143,7 +149,7 @@ public class ChiTietActivity extends AppCompatActivity {
         super.onResume();
         if (Utils.gioHangList != null) {
             int totalItems = 0;
-            for (int i = 0; i < Utils.gioHangList.size(); i++){
+            for (int i = 0; i < Utils.gioHangList.size(); i++) {
                 totalItems += Utils.gioHangList.get(i).getSoluong();
             }
             badgeChiTiet.setText(String.valueOf(totalItems));
